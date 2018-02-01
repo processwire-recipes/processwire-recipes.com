11dfa61726ec42874b2c13a9b3c710a304b4f533
Marcus Herrmann <mail@marcus-herrmann.com>
2018-02-01 15:26:36 +0100

    Update Piwik/Matomo

--- site/templates/partials/footer.php
+++ site/templates/partials/footer.php
@@ -10,21 +10,22 @@
 		</footer>
 	<script type="text/javascript" src="{{ templates }}build/js/pwr.js"></script>
 
-	<!-- Piwik -->
+	<!-- Matomo -->
 	<script type="text/javascript">
-		var _paq = _paq || [];
-		_paq.push(['trackPageView']);
-		_paq.push(['enableLinkTracking']);
-		(function() {
-			var u="//piwik.marcus-herrmann.com/";
-			_paq.push(['setTrackerUrl', u+'piwik.php']);
-			_paq.push(['setSiteId', 6]);
-			var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-			g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-		})();
+	  var _paq = _paq || [];
+	  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+	  _paq.push(['trackPageView']);
+	  _paq.push(['enableLinkTracking']);
+	  (function() {
+	    var u="//stat.marcus.io/";
+	    _paq.push(['setTrackerUrl', u+'piwik.php']);
+	    _paq.push(['setSiteId', '4']);
+	    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+	    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+	  })();
 	</script>
-	<noscript><p><img src="//piwik.marcus-herrmann.com/piwik.php?idsite=6" style="border:0;" alt="" /></p></noscript>
-	<!-- End Piwik Code -->
+	<!-- End Matomo Code -->
+
 
 
 	</body>
